4a92920b1c5bb4748a7cdb970ccfec85a2df27a6
Raghav Sharma <raghav_tinker@yahoo.com>
2021-04-18 15:33:31 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,126 @@
+from discord.ext import commands
+import os
+import asyncio
+import random
+import discord
+from sps import computerChoose
+from sps import writeExcelSheet
+
+options = ["scissors", "paper", "stone"]
+prefix = "&"
+client = commands.Bot(command_prefix=prefix, case_insensitive=True)
+
+user_score = 0
+computer_score = 0
+
+def compete(user, computer):
+    global user_score
+    global computer_score
+
+    print("User choice: {}\tMy choice: {}".format(user, computer))
+
+    #conditions where the user will win
+    if(user == "scissors" and computer == "paper"):
+        user_score = user_score + 1
+        return "You won!"
+
+    elif(user == "stone" and computer == "scissors"):
+        user_score = user_score + 1
+        return "You won!"
+
+    elif(user == "paper" and computer == "stone"):
+        user_score = user_score + 1
+        return "You won!"
+    #=============================================
+    #conditions where the computer would win
+
+    elif(user == "scissors" and computer == "stone"):
+        computer_score = computer_score + 1
+        return "I won!"
+        
+        
+    elif(user == "paper" and computer == "scissors"):
+        computer_score = computer_score + 1
+        return "I won!"
+        
+
+    elif(user == "stone" and computer == "paper"):
+        computer_score = computer_score + 1
+        return "I won!"
+        
+
+    else:
+        computer_score = computer_score + 1
+        user_score = user_score + 1
+        return "Its a draw!"
+
+@client.command(name="ping")
+async def _ping(ctx):
+    await ctx.send(f"Ping: ```{client.latency}```")
+
+@client.command(name="sayhello")
+async def sayHello(ctx):
+    await ctx.send("Hi! Please type in your name!")
+
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+
+    msg = await client.wait_for("message", check=check)
+    await ctx.send("Hi " + msg.content + "!")
+    print(msg.content)
+
+
+
+@client.command(name="sps")
+async def sps(ctx):
+    global user_score
+    global computer_score
+
+    await ctx.send("Lets place stone paper scissor!!!")
+    await ctx.send("Number of rounds?")
+
+    def check(rounds_str):
+        return rounds_str.author == ctx.author and rounds_str.channel == ctx.channel
+    
+    rounds_str = await client.wait_for("message", check=check)
+
+    rounds = 0
+    try:
+        rounds = int(rounds_str.content)
+    except:
+        await ctx.send("Enter a valid integer")
+    
+    count = 0
+    user_score = 0
+    computer_score = 0
+
+    while count < rounds:
+        await ctx.send("ROUND " + str(count + 1) + " \nEnter ```[stone, paper or scissors]```")
+
+        def checkOption(option):
+            return option.author == ctx.author and option.channel == ctx.channel
+        
+        option  = await client.wait_for("message", check=checkOption)
+
+        computer_choice = computerChoose()
+
+        if option.content in options:
+            result = compete(option.content, computer_choice)
+            await ctx.send("```Your choice: {}\n My choice: {}```\n{}".format(option.content, computer_choice, result))
+            count = count + 1
+        else:
+            await ctx.send("Enter a valid option")
+            await ctx.send("Retry!")
+            pass
+    
+    await ctx.send("Competition over!")
+    await ctx.send("SCOREBOARD")
+    await ctx.send("```Your score = {}\nMy score = {}```".format(user_score, computer_score))
+    writeExcelSheet(user_score, computer_score)
+
+@client.command(name="sps_hist")
+async def sps_histSend(ctx):
+    await ctx.send("Here are all my game results till now")
+    await ctx.send(file=discord.File("score.xlsx"))
+
+client.run(os.getenv('TOKEN'))
---
+++ sps.py
@@ -0,0 +1,41 @@
+import random
+import openpyxl
+import datetime
+from os import path
+
+options = ["scissors", "paper", "stone"]
+
+#Writes score of game in excel sheet
+def writeExcelSheet(user_score, computer_score):
+
+    #getting date and also the result of the game
+    date = (str(datetime.datetime.now()).split("."))[0]
+    result = ""
+    if(user_score > computer_score):
+        result = "User won"
+    elif(user_score == computer_score):
+        result = "Draw"
+    else:
+        result = "Computer won"
+
+    #checking if the excel file exists
+    if(path.exists("score.xlsx")):
+        work_book = openpyxl.load_workbook("score.xlsx")
+        work_sheet = work_book.active
+        work_sheet.append([date, result, user_score, computer_score])
+        work_book.save("score.xlsx")
+    else:
+        #if no then create one put the headings and recall this function
+        wb = openpyxl.Workbook()
+        sheet = wb.active
+        sheet.cell(1,1).value = "Date_Time"
+        sheet.cell(1,2).value = "Result"
+        sheet.cell(1,3).value = "UserScore"
+        sheet.cell(1,4).value = "ComputerScore"
+        wb.save("score.xlsx")
+        writeExcelSheet(user_score, computer_score)
+
+
+def computerChoose():
+    choice = random.randint(0, len(options)-1)
+    return options[choice]
